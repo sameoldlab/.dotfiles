677c99647efaac2c6889544a0485c85aae90ba1b
ibro <sameoldlab@proton.me>
2023-10-09 15:53:13 +0100

    back to zsh

--- fish/config.fish
+++ fish/config.fish
@@ -6,3 +6,11 @@ nvm use default --silent
 
 starship init fish | source
 zoxide init fish | source
+
+# >>> conda initialize >>>
+# !! Contents within this block are managed by 'conda init' !!
+if test -f /home/ibro/miniconda3/bin/conda
+    eval /home/ibro/miniconda3/bin/conda "shell.fish" "hook" $argv | source
+end
+# <<< conda initialize <<<
+
