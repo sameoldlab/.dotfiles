bbc1ca9032cf3d68f8c7e3ab011f72de398bb68c
ibro <sameoldlab@proton.me>
2024-04-17 18:50:15 +0100

    update setup.sh

--- ags/setup.sh
+++ ags/setup.sh
@@ -1,34 +1,35 @@
 #!/usr/bin/env bash
 
-dir="${1:-$HOME/.config/ags}"
+DATA_DIR="$DESTDIR/$1"
+PKGDATA_DIR="$DESTDIR/$2"
+BIN_DIR="$DESTDIR/$3"
+APP_ID=$4
 
-mkdir -p /tmp/ags-config
-cd /tmp/ags-config
+mkdir -p $BIN_DIR
 
-# clone
-echo "cloning ags"
-git clone https://github.com/Aylur/ags.git
-cd ags
-npm i
+BIN_SRC="$PKGDATA_DIR/$APP_ID"
+BIN_DEST="$BIN_DIR/ags"
+ln -s -f $BIN_SRC $BIN_DEST
 
-# generate
-echo "generating types..."
-tsc -d --declarationDir dts --emitDeclarationOnly
+if [[ "$5" == "false" ]]; then
+	exit 0
+fi
 
-# fix paths
-find ./dts -type f | xargs sed -i 's/gi:\/\/Gtk?version=3.0/node_modules\/@girs\/gtk-3.0\/gtk-3.0/g'
-find ./dts -type f | xargs sed -i 's/gi:\/\/GObject/node_modules\/@girs\/gobject-2.0\/gobject-2.0/g'
-find ./dts -type f | xargs sed -i 's/gi:\/\/Gio/node_modules\/@girs\/gio-2.0\/gio-2.0/g'
-find ./dts -type f | xargs sed -i 's/gi:\/\/GLib/node_modules\/@girs\/glib-2.0\/glib-2.0/g'
-find ./dts -type f | xargs sed -i 's/gi:\/\/GdkPixbuf/node_modules\/@girs\/gdkpixbuf-2.0\/gdkpixbuf-2.0/g'
-find ./dts -type f | xargs sed -i 's/gi:\/\/Gdk/node_modules\/@girs\/gdk-2.0\/gdk-2.0/g'
-find ./dts -type f | xargs sed -i 's/gi:\/\/Gvc/node_modules\/@girs\/gvc-1.0\/gvc-1.0/g'
-find ./dts -type f | xargs sed -i 's/gi:\/\/NM/node_modules\/@girs\/nm-1.0\/nm-1.0/g'
-find ./dts -type f | xargs sed -i 's/gi:\/\/DbusmenuGtk3/node_modules\/@girs\/dbusmenugtk3-0.4\/dbusmenugtk3-0.4/g'
+SRC=$6
+TYPES="$PKGDATA_DIR/types"
 
-# move
-mv dts $dir/types
-echo "types moved to $dir/types"
+mkdir -p $TYPES
+
+tsc -p $SRC/tsconfig.json -d --declarationDir $TYPES --emitDeclarationOnly
+
+function fixPaths {
+	sed -i 's/node_modules/types/g' $1
+	sed -i 's/import("@girs/import("types\/@girs/g' $1
+}
+
+export -f fixPaths
+find $TYPES -type f | xargs -I % bash -c "fixPaths %"
+cp -r "$SRC/node_modules/@girs" "$TYPES/@girs"
 
 # gen ags.d.ts
 function mod {
@@ -42,49 +43,31 @@ function resource {
 	mod "resource:///com/github/Aylur/ags/$1.js" "./$1"
 }
 
-function gi {
-	mod "gi://$1" "node_modules/@girs/$2/$2"
-}
+dts="$TYPES/ags.d.ts"
 
-dts="$dir/types/ags.d.ts"
-
-echo "
-declare function print(...args: any[]): void;
-
-declare module console {
-    export function error(obj: object, others?: object[]): void;
-    export function error(msg: string, subsitutions?: any[]): void;
-    export function log(obj: object, others?: object[]): void;
-    export function log(msg: string, subsitutions?: any[]): void;
-    export function warn(obj: object, others?: object[]): void;
-    export function warn(msg: string, subsitutions?: any[]): void;
-}
+echo "declare function print(...args: any[]): void;
+declare const Widget: typeof import('./widget').default
+declare const Service: typeof import('./service').default
+declare const Variable: typeof import('./variable').default
+declare const Utils: typeof import('./utils').default
+declare const App: typeof import('./app').default
 " >$dts
 
-for file in ./src/*.ts; do
+for file in $SRC/src/*.ts; do
 	f=$(basename -s .ts $file)
 	if [[ "$f" != "main" && "$f" != "client" ]]; then
 		resource "$(basename -s .ts $file)" >>$dts
 	fi
 done
 
-for file in ./src/service/*.ts; do
+for file in $SRC/src/service/*.ts; do
 	resource "service/$(basename -s .ts $file)" >>$dts
 done
 
-for file in ./src/widgets/*.ts; do
+for file in $SRC/src/widgets/*.ts; do
 	resource "widgets/$(basename -s .ts $file)" >>$dts
 done
 
-gi "Gtk" "gtk-3.0" >>$dts
-gi "GObject" "gobject-2.0" >>$dts
-gi "Gio" "gio-2.0" >>$dts
-gi "GLib" "glib-2.0" >>$dts
-
-# remove tmp
-# rm -rf /tmp/ags-config
-
-# npm i
-echo "npm install @girs"
-cd $dir
-npm i
+for file in $SRC/src/utils/*.ts; do
+	resource "utils/$(basename -s .ts $file)" >>$dts
+done
