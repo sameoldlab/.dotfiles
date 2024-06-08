c7d2cfae7cb9e4d3637e9a99e1c8c126f38b1060
ibro <sameoldlab@proton.me>
2024-06-08 23:41:22 +0100

    niri has named workspaces now :)

--- ags/base/Bar/index.ts
+++ ags/base/Bar/index.ts
@@ -16,20 +16,20 @@ export const Clock = () =>
 		spacing: 4,
 		children: [
 			Widget.Label({
-				label: date.bind().as(d => d.format('%I:%M %p')?.toString() ?? ''),
-				class_name: 'time',
-				justification: 'right',
-				// vpack: 'end',
-				vexpand: true,
-				// hexpand: true,
-			}),
-			Widget.Label({
-				label: date.bind().as(d => d.format('%a, %b %d')?.toString() ?? ''),
+				label: date.bind().as(d => d.format('%a %d ')?.toString() ?? ''),
 				class_name: 'date',
 				// hexpand: true,
 				vexpand: true,
 				// vpack: 'end',
 				justification: 'right',
+			}),
+			Widget.Label({
+				label: date.bind().as(d => d.format('%I:%M')?.toString() ?? ''),
+				class_name: 'time',
+				justification: 'right',
+				// vpack: 'end',
+				vexpand: true,
+				// hexpand: true,
 			}),
 		],
 	})
@@ -52,30 +52,70 @@ const systemTray = () =>
 	Widget.Box({
 		children: SystemTray.bind('items').as(items =>
 			items.map(item => Widget.Button({
-					child: Widget.Icon({
-						 icon: item.bind('icon'),
-						}),
-					class_name: "tray-icon",
-					on_primary_click: (_, event) => item.activate(event),
-					on_secondary_click: (_, event) => item.openMenu(event),
-					tooltip_markup: item.bind('tooltip_markup'),
-				})
+				child: Widget.Icon({
+					icon: item.bind('icon'),
+				}),
+				class_name: "tray-icon",
+				on_primary_click: (_, event) => item.activate(event),
+				on_secondary_click: (_, event) => item.openMenu(event),
+				tooltip_markup: item.bind('tooltip_markup'),
+			})
 			)
-		), 
+		),
 	})
 /*
  */
 
+const Workspaces = () => Widget.Box({
+	className: 'workspaces',
+	child: Widget.EventBox({
+		on_scroll_up: () => Utils.execAsync('niri msg action focus-workspace-up'),
+		on_scroll_down: () => Utils.execAsync('niri msg action focus-workspace-down'),
+		// on_scroll_left: ()=> Utils.execAsync('niri msg action focus-column-left'),
+		child: Widget.Box({
+			children: [...Array(10)].map((_, i) => {
+				i++
+				return Widget.Button({
+					className: 'occupied',
+					on_clicked: () => Utils.execAsync(`niri msg action focus-workspace ${i}`).catch(print),
+					child: Widget.Label({
+						label: `${i}`,
+						className: 'indicator',
+					}),
+					// connections: [[Hyprland, btn => {
+					// 	btn.toggleClassName('focused', Hyprland.active.workspace.id === i)
+					// 	btn.toggleClassName('occupied', Hyprland.getWorkspace(i)?.windows > 0)
+					// }]]
+				})
+			})
+		})
+	})
+})
+
+const Current = () => {
+	const label = Variable('', {
+		poll: [1800, 'niri msg -j focused-window', (res: string) => {
+			// print(res)
+			const focused = JSON.parse(res)
+
+			return `${focused.title.substring(0,24)} | ${focused.app_id}`
+		}]
+	})
+
+	return Widget.Label({
+		label: label.bind()})
+}
 // layout of the bar
 const Left = Widget.Box({
 	children: [
-		//Workspaces()
+		Workspaces(),
 	],
 })
 
 export const Center = Widget.Box({
 	children: [
 		// Notification(),
+		Current()
 	],
 })
 
