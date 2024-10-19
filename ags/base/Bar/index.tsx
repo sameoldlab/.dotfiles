e0a988e87bd4cb30b1cfefddeabae2a24805d545
ibro <sameoldlab@proton.me>
2024-10-19 20:46:54 +0100

    convert example bar to widget constructor

--- ags/base/Bar/index.tsx
+++ ags/base/Bar/index.tsx
@@ -1,29 +1,35 @@
-import { App, Astal, Gtk } from "astal/gtk3"
-import { Variable } from "astal"
+import { App, Astal, Gtk, Widget } from "astal/gtk3"
+import { bind, Variable } from "astal"
 
-const time = Variable("").poll(1000, "date")
+const date = Variable("").poll(1000, "date")
 
-export default (monitor: number) => {
-  return <window
-    className="Bar"
-    monitor={monitor}
-    exclusivity={Astal.Exclusivity.EXCLUSIVE}
-    anchor={Astal.WindowAnchor.TOP
-      | Astal.WindowAnchor.LEFT
-      | Astal.WindowAnchor.RIGHT}
-    application={App}>
-    <centerbox>
-      <button
-        onClicked="echo hello"
-        halign={Gtk.Align.CENTER} >
-        Welcome to AGS!
-      </button>
-      <box />
-      <button
-        onClick={() => print("hello")}
-        halign={Gtk.Align.CENTER} >
-        <label label={time()} />
-      </button>
-    </centerbox>
-  </window>
+export function Bar(monitor = 0) {
+  return new Widget.Window(
+    {
+      className: "Bar",
+      monitor: monitor,
+      // exclusivity={Astal.Exclusivity.EXCLUSIVE}
+      anchor: Astal.WindowAnchor.TOP
+        | Astal.WindowAnchor.LEFT
+        | Astal.WindowAnchor.RIGHT,
+      application: App
+    },
+    new Widget.CenterBox({
+      centerWidget:
+        new Widget.Button({
+          onClicked: "echo hello",
+          halign: Gtk.Align.CENTER
+        },
+          new Widget.Label({ label: 'Welcome to AGS!' })
+        ),
+      endWidget:
+        new Widget.Button({
+          onClicked: () => print("hello"),
+          halign: Gtk.Align.CENTER
+        },
+          new Widget.Label({ label: bind(date).as(v => v) })
+        )
+    },
+    )
+  )
 }
