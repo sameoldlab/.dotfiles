176770b8c2363f6bf050b8388c311a59bcd00e8b
ibro <sameoldlab@proton.me>
2024-10-21 07:17:31 +0100

    change entry file

--- ags/config.js
+++ ags/config.js
@@ -1,10 +1,10 @@
 const outdir = App.configDir + '/build'
 console.log(outdir)
 try {
-	await import(`file://${outdir}/base/main.js`)
+	await import(`file://${outdir}/main.js`)
 } catch (error) {
 	console.error(error)
 	console.warn(`run tsc or pnpm build in ${App.configDir} to build the config`)
 }
 
-export {}
+export { }
