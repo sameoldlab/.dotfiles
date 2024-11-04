dc3fc4c5a203e50bf31051f28a09f1c73253f655
ibro <sameoldlab@proton.me>
2024-11-04 14:43:12 +0100

    convert Workspaces, Current, SystemTray, and Battery widgets

--- ags/base/Bar/index.ts
+++ ags/base/Bar/index.ts
@@ -2,40 +2,67 @@
 // import SystemTray from 'resource:///com/github/Aylur/ags/service/systemtray.js'
 // import Media from './media.js'
 // import SysTray from './systray.js'
-import { App, Astal, Gtk, Widget } from "astal/gtk3"
+import SystemTray from 'gi://AstalTray'
+import Battery from 'gi://AstalBattery'
+import Niri from 'gi://AstalNiri'
+import { App, Astal, Gtk, Gdk, Widget } from "astal/gtk3"
 import { bind, Variable } from "astal"
 import * as Utils from 'astal/process'
 
+const log = (args: Array<any>) => console.log(args)
+
+const batteryLabel = () => {
+	const bat = Battery.get_default()
+	return new Widget.Box({
+		spacing: 2,
+		vexpand: true,
+		class_name: 'battery tray-icon',
+		tooltip_text: bind(bat, 'time_to_empty').as(s => `naturalTime: ${s}`)
+	},
+		new Widget.Icon({
+			icon: bind(bat, 'battery_icon_name',),
+		}),
+		bind(bat, 'percentage').as(p => Math.round(p * 100) + '%')
+	)
+}
+
 const date = Variable(new Date(0)).poll(1000, "date -R", (d) => new Date(d))
 
+let hide = false
 export const Clock = () =>
-	new Widget.Box({
-		className: 'clock',
-		vertical: false,
-		spacing: 4,
+	new Widget.Button({
+		onClick(self, event) {
+			hide = !hide
+			self.toggleClassName('hide', hide)
+		},
 	},
-		new Widget.Label({
-			label: bind(date).as(d => {
-				const date = d.toDateString().split(' ')
-				return `${date[0]} ${date[1]} ${date[2]}` ?? ''
+		new Widget.Box({
+			className: 'clock',
+			vertical: false,
+			spacing: 4,
+		},
+			new Widget.Label({
+				label: bind(date).as(d => {
+					const date = d.toDateString().split(' ')
+					return `${date[0]} ${date[1]} ${date[2]}` ?? ''
+				}),
+				className: 'date',
+				// hexpand: true,
+				vexpand: true,
+				// vpack: 'end',
+				halign: Gtk.Align.END,
+				// justification: 'right',
 			}),
-			className: 'date',
-			// hexpand: true,
-			vexpand: true,
-			// vpack: 'end',
-			halign: Gtk.Align.END,
-			// justification: 'right',
-		}),
-		new Widget.Label({
-			label: bind(date).as(d => d.toTimeString().split(' ', 1)[0] ?? ''),
-			className: 'time',
-			halign: Gtk.Align.END,
-			// justification: 'right',
-			// vpack: 'end',
-			vexpand: true,
-			// hexpand: true,
-		}),
-	)
+			new Widget.Label({
+				label: bind(date).as(d => d.toTimeString().split(' ', 1)[0] ?? ''),
+				className: 'time',
+				halign: Gtk.Align.END,
+				// justification: 'right',
+				// vpack: 'end',
+				vexpand: true,
+				// hexpand: true,
+			}),
+		))
 
 /*
 const Notification = () =>
@@ -51,66 +78,118 @@ const Notification = () =>
 			}),
 		],
 	})
-
-export const systemTray = (opts: {vertical: boolean}) =>
-	Widget.Box({
-		vertical: opts.vertical,
-		children: SystemTray.bind('items').as(items =>
-			items.map(item => Widget.Button({
-				child: Widget.Icon({
-					icon: item.bind('icon'),
-				}),
-				class_name: "tray-icon",
-				on_primary_click: (_, event) => item.activate(event),
-				on_secondary_click: (_, event) => item.openMenu(event),
-				tooltip_markup: item.bind('tooltip_markup'),
-			})
-			)
-		),
-	})
-
-
-	return Widget.Label({
-		label: label.bind()})
-}
 */
 
-export const Workspaces = (opts: { vertical: boolean }) => new Widget.Box({
-	className: 'workspaces',
-	child: new Widget.EventBox({
-		// on_scroll_up: () => Utils.execAsync('niri msg action focus-workspace-up'),
-		// on_scroll_down: () => Utils.execAsync('niri msg action focus-workspace-down'),
-		// on_scroll_left: ()=> Utils.execAsync('niri msg action focus-column-left'),
-		child: new Widget.Box({
-			vertical: opts.vertical,
-			children: [...Array(10)].map((_, i) => {
-				i++
-				return new Widget.Button({
-					className: 'occupied',
-					on_clicked: () => Utils.execAsync(`niri msg action focus-workspace ${i}`).catch(print),
-					child: new Widget.Label({
-						label: `${i}`,
-						className: 'indicator',
-					}),
-					// connections: [[Hyprland, btn => {
-					// 	btn.toggleClassName('focused', Hyprland.active.workspace.id === i)
-					// 	btn.toggleClassName('occupied', Hyprland.getWorkspace(i)?.windows > 0)
-					// }]]
-				})
-			})
-		})
+export const systemTray = (opts: { vertical: boolean }) => {
+	const tray = SystemTray.get_default()
+	const child = bind(tray, 'items').as(items => items.map(item => {
+		let menu = item.create_menu()
+		return new Widget.Button({
+			class_name: "tray-icon",
+			tooltip_markup: bind(item, 'tooltip_markup'),
+			onDestroy(self) {
+				menu?.destroy()
+			},
+			on_click: (btn, event) => {
+				switch (event.button) {
+					case Gdk.BUTTON_PRIMARY:
+						return item.activate(event.x, event.y)
+					case Gdk.BUTTON_SECONDARY:
+						log('right click')
+						return menu?.popup_at_widget(btn, Gdk.Gravity.NORTH, Gdk.Gravity.SOUTH, null)
+						break
+					case Gdk.BUTTON_MIDDLE:
+						return item.secondary_activate(event.x, event.y)
+				}
+			}
+		},
+			new Widget.Icon({ g_icon: bind(item, 'gicon') })
+		)
+	}))
+
+	return new Widget.Box({
+		vertical: opts.vertical,
+	},
+		child)
+}
+
+export const Workspaces = (opts: { vertical: boolean }) => {
+	const workspaces: Variable<Niri.Workspace[]> = Variable([])
+	const niri = Niri.get_default()
+	const active = Variable('')
+	niri.connect('event', () => {
+		const ws = niri.get_workspaces()
+		// log(ws.map(w => w.id))
+		workspaces.set(ws)
 	})
-})
+
+	// Mod keys on my logitech mx master using wayland in 2024
+	// Hardcoded because I can't find the right value in Gdk.ModifierType
+	// Check event.modifier if setup changes 
+	const BACK = 32768
+	const FORWARD = 65536
+
+	let lastTime = 0
+	return new Widget.Box({
+		className: 'workspaces',
+	},
+		new Widget.EventBox({
+			onScroll(self, event) {
+				if (event.time - lastTime < 150) return
+				lastTime = event.time
+
+				switch (event.direction) {
+					case Gdk.ScrollDirection.UP: return Utils.execAsync('niri msg action focus-workspace-up')
+					case Gdk.ScrollDirection.DOWN: return Utils.execAsync('niri msg action focus-workspace-down')
+					case Gdk.ScrollDirection.LEFT: return Utils.execAsync('niri msg action focus-column-left')
+					case Gdk.ScrollDirection.RIGHT: return Utils.execAsync('niri msg action focus-column-right')
+					case Gdk.ScrollDirection.SMOOTH:
+						const isVerical = Math.abs(event.delta_y) === Math.max(Math.abs(event.delta_x), Math.abs(event.delta_y))
+						if (isVerical) {
+							if (event.delta_y < 0)
+								return Utils.execAsync('niri msg action focus-workspace-up')
+							return Utils.execAsync('niri msg action focus-workspace-down')
+						}
+						if (event.delta_x < 0)
+							return Utils.execAsync('niri msg action focus-column-left')
+						return Utils.execAsync('niri msg action focus-column-right')
+				}
+			},
+		},
+			new Widget.Box({
+				vertical: opts.vertical
+			},
+				bind(workspaces).as(wksp => wksp.filter((w) => w.is_focused).map((w, i) => new Widget.Button({
+					className: 'occupied indicator',
+					on_clicked: () => Utils.execAsync(`niri msg action focus-workspace ${w.id}`).catch(print),
+				}, w.name
+				)))
+			)
+		)
+	)
+}
+
 
 const Current = () => {
-	const label = Variable('')
-		.poll(1800, 'niri msg -j focused-window', (res: string) => {
-			// print(res)
-			const focused = JSON.parse(res)
-
-			return `${focused.title.substring(0, 24)} | ${focused.app_id}`
+	const niri = Niri.get_default()
+	const active = Variable('')
+	niri.connect('window-focus-changed', (_, w) => {
+		w && active.set(w.title)
+		w?.connect('changed', () => {
+			w && active.set(w.title)
 		})
-	return new Widget.Label({ label: bind(label) })
+	})
+	niri.connect('window-opened', (_, w) => {
+		w && active.set(w.title)
+		w?.connect('changed', () => {
+			w && active.set(w.title)
+		})
+	})
+
+	return new Widget.Label({
+		onDestroy: () => active.drop(),
+		label: bind(active)
+	})
 }
 // layout of the bar
 const Left = new Widget.Box({
@@ -130,11 +209,14 @@ export const Right = new Widget.Box({
 	halign: Gtk.Align.END,
 	// hpack: 'end',
 	children: [
+
 		/*
 		Media(),
-		systemTray({vertical: false}),
-		SysTray(), 
-		*/
+		SysTray(),
+	'	*/
+		systemTray({ vertical: false }),
+		// sysTray(),
+		batteryLabel(),
 		Clock(),
 	],
 })
