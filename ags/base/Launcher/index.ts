f2f0f8f57ed548c07f2d91d16886d9159549ca71
ibro <sameoldlab@proton.me>
2024-11-06 08:54:16 +0100

    extract ListItem to separate file

--- ags/base/Launcher/index.ts
+++ ags/base/Launcher/index.ts
@@ -4,61 +4,12 @@ import { bind, Variable } from "astal"
 import { execAsync } from 'astal/process'
 import Launcher from '../services/launcherIPC.js'
 import type { JsonIPC } from '../services/launcherIPC.js'
+import { ListItem } from './ListItem'
 const WINDOW_NAME = 'poplauncher'
 const launcher = Launcher.get_default()
 
 let active_id = 0
-const EntryItem = (res: JsonIPC.SearchResult) =>
-	new Widget.EventBox({
-		on_primary_click: () => launcher.activate(res.id),
 
-		className: 'applauncher__item',
-		name: res.name,
-		child: new Widget.Box({
-			children: [
-				new Widget.Icon({
-					icon: res.category_icon
-						? 'Name' in res.category_icon
-							? res.category_icon.Name
-							: Gio.content_type_get_icon(res.category_icon.Mime).names[1]
-						: '',
-					size: 16,
-					className: 'icon category_icon',
-				}),
-				new Widget.Icon({
-					icon: res.icon
-						? 'Name' in res.icon
-							? res.icon.Name
-							: Gio.content_type_get_icon(res.icon.Mime).names[1]
-						: '',
-					size: 28,
-					className: 'icon',
-				}),
-				new Widget.Box({
-					vertical: true,
-					vpack: 'center',
-					children: [
-						new Widget.Label({
-							class_name: 'title',
-							label: res.name,
-							xalign: 0,
-							vpack: 'center',
-							truncate: true,
-						}),
-						// short circuit if there is no description
-						new Widget.Label({
-							class_name: 'description',
-							label: res.description || '',
-							wrap: true,
-							xalign: 0,
-							justification: 'left',
-							vpack: 'center',
-						}),
-					],
-				}),
-			],
-		}),
-	})
 
 const entries = Variable<JsonIPC.SearchResult[]>([])
 
@@ -71,7 +22,7 @@ const close = () => {
 }
 const list = new Widget.Box({
 	vertical: true,
-	children: bind(entries).as(v => v.map(EntryItem)),
+	children: bind(entries).as(v => v.map(ListItem)),
 	spacing: 0,
 })
 
