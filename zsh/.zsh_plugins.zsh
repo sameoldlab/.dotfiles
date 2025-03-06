a39405c30a161be586ecef47c71d894cd20638ae
ibro <sameoldlab@proton.me>
2025-03-06 22:04:00 +0100

    completions finally

--- zsh/.zsh_plugins.zsh
+++ zsh/.zsh_plugins.zsh
@@ -1,5 +1,8 @@
-fpath+=( /home/ibro/.cache/antidote/mattmc3/zephyr/plugins/completion )
-source /home/ibro/.cache/antidote/mattmc3/zephyr/plugins/completion/completion.plugin.zsh
+fpath+=( /home/ibro/.cache/antidote/mattmc3/ez-compinit )
+source /home/ibro/.cache/antidote/mattmc3/ez-compinit/ez-compinit.plugin.zsh
+fpath+=( /home/ibro/.cache/antidote/zsh-users/zsh-completions/src )
+fpath+=( /home/ibro/.cache/antidote/aloxaf/fzf-tab )
+source /home/ibro/.cache/antidote/aloxaf/fzf-tab/fzf-tab.plugin.zsh
 fpath+=( /home/ibro/.cache/antidote/mattmc3/zephyr/plugins/color )
 source /home/ibro/.cache/antidote/mattmc3/zephyr/plugins/color/color.plugin.zsh
 source $ZDOTDIR/aliases
@@ -12,13 +15,7 @@ fi
 zsh-defer source $ZDOTDIR/plugins/fzf-tab-completions.zsh
 source $ZDOTDIR/plugins/dirstack.zsh
 source $ZDOTDIR/plugins/editor.zsh
-fpath+=( /home/ibro/.cache/antidote/belak/zsh-utils/completion )
-source /home/ibro/.cache/antidote/belak/zsh-utils/completion/completion.plugin.zsh
-fpath+=( /home/ibro/.cache/antidote/Aloxaf/fzf-tab )
-source /home/ibro/.cache/antidote/Aloxaf/fzf-tab/fzf-tab.plugin.zsh
 fpath+=( /home/ibro/.cache/antidote/zdharma-continuum/fast-syntax-highlighting )
 zsh-defer source /home/ibro/.cache/antidote/zdharma-continuum/fast-syntax-highlighting/fast-syntax-highlighting.plugin.zsh
 fpath+=( /home/ibro/.cache/antidote/zsh-users/zsh-autosuggestions )
 zsh-defer source /home/ibro/.cache/antidote/zsh-users/zsh-autosuggestions/zsh-autosuggestions.plugin.zsh
-fpath+=( /home/ibro/.cache/antidote/zsh-users/zsh-history-substring-search )
-zsh-defer source /home/ibro/.cache/antidote/zsh-users/zsh-history-substring-search/zsh-history-substring-search.plugin.zsh
